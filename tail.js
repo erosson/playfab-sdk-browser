385c752af9434488816643de58de84bc22acacf4
Evan <github@swarmsim.com>
2016-10-15 02:38:42 +0000

    init

---
+++ tail.js
@@ -0,0 +1,7 @@
+
+
+// above downloaded from https://download.playfab.com/PlayFabClientApi.js
+// below added by hand for this node package
+var exports = module.exports;
+exports.PlayFab = PlayFab;
+exports.PlayFabClientSDK = PlayFabClientSDK;
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -eux
+wget https://download.playfab.com/PlayFabClientApi.js
+cat tail.js >> PlayFabClientApi.js
